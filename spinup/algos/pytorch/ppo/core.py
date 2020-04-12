766611473d634aa7de2604632de3b94e2128a7e0
Mamat Shamshiev <mamat97@gmail.com>
2020-04-12 20:03:41 +0300

    Added 2-headed Actor-Critic

--- spinup/algos/pytorch/ppo/core.py
+++ spinup/algos/pytorch/ppo/core.py
@@ -133,6 +133,37 @@ class MLPActorCritic(nn.Module):
         return self.step(obs)[0]
 
 
+class MLPActorCritic2Heads(nn.Module):
+
+    def __init__(self, observation_space, action_space,
+                 hidden_sizes=(64, 64), activation=nn.Tanh):
+        super().__init__()
+
+        obs_dim = observation_space.shape[0]
+
+        # policy builder depends on action space
+        if isinstance(action_space, Box):
+            self.pi = MLPGaussianActor(obs_dim, action_space.shape[0], hidden_sizes, activation)
+        elif isinstance(action_space, Discrete):
+            self.pi = MLPCategoricalActor(obs_dim, action_space.n, hidden_sizes, activation)
+
+        # build value functions for extristic and intristic rewards
+        self.v_extr = MLPCritic(obs_dim, hidden_sizes, activation)
+        self.v_intr = MLPCritic(obs_dim, hidden_sizes, activation)
+
+    def step(self, obs):
+        with torch.no_grad():
+            pi = self.pi._distribution(obs)
+            a = pi.sample()
+            logp_a = self.pi._log_prob_from_distribution(pi, a)
+            v_extr = self.v_extr(obs)
+            v_intr = self.v_intr(obs)
+        return a.numpy(), v_extr.numpy(), v_intr.numpy(), logp_a.numpy()
+
+    def act(self, obs):
+        return self.step(obs)[0]
+
+
 class RND(nn.Module):
     def __init__(self, obs_dim, hidden_sizes, activation):
         super().__init__()
