22cd06de2a3f2b40d46586071ba7808d705fdb82
Mamat Shamshiev <mamat97@gmail.com>
2020-06-02 12:36:14 +0300

    Added ppo_rnd

--- spinup/__init__.py
+++ spinup/__init__.py
@@ -1,7 +1,14 @@
 # Disable TF deprecation warnings.
 # Syntax from tf1 is not expected to be compatible with tf2.
 import tensorflow as tf
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+from spinup.algos.pytorch.ddpg.ddpg import ddpg as ddpg_pytorch
+from spinup.algos.pytorch.ppo.ppo import ppo as ppo_pytorch
+from spinup.algos.pytorch.ppo.ppo_rnd import ppo as ppo_rnd_pytorch
+from spinup.algos.pytorch.sac.sac import sac as sac_pytorch
+from spinup.algos.pytorch.td3.td3 import td3 as td3_pytorch
+from spinup.algos.pytorch.trpo.trpo import trpo as trpo_pytorch
+from spinup.algos.pytorch.vpg.vpg import vpg as vpg_pytorch
 
 # Algorithms
 from spinup.algos.tf1.ddpg.ddpg import ddpg as ddpg_tf1
@@ -11,15 +18,10 @@ from spinup.algos.tf1.td3.td3 import td3 as td3_tf1
 from spinup.algos.tf1.trpo.trpo import trpo as trpo_tf1
 from spinup.algos.tf1.vpg.vpg import vpg as vpg_tf1
 
-from spinup.algos.pytorch.ddpg.ddpg import ddpg as ddpg_pytorch
-from spinup.algos.pytorch.ppo.ppo import ppo as ppo_pytorch
-from spinup.algos.pytorch.sac.sac import sac as sac_pytorch
-from spinup.algos.pytorch.td3.td3 import td3 as td3_pytorch
-from spinup.algos.pytorch.trpo.trpo import trpo as trpo_pytorch
-from spinup.algos.pytorch.vpg.vpg import vpg as vpg_pytorch
-
 # Loggers
-from spinup.utils.logx import Logger, EpochLogger
+from spinup.utils.logx import EpochLogger, Logger
 
 # Version
 from spinup.version import __version__
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
